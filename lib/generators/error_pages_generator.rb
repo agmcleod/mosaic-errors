5168c59031d3dcc6aa546c961b3ed8fc225c54f8
Kel Stopper <kel.stopper@mosaic.com>
2011-06-30 12:06:22 -0400

    place errors in errors folder not layouts -- doh

--- lib/generators/error_pages_generator.rb
+++ lib/generators/error_pages_generator.rb
@@ -2,7 +2,7 @@ class ErrorPagesGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
   
   def copy_error_pages
-    copy_file "internal_server_error.html.haml", "app/views/layouts/internal_server_error.html.haml"
-    copy_file "not_found.html.haml", "app/views/layouts/not_found.html.haml"
+    copy_file "internal_server_error.html.haml", "app/views/errors/internal_server_error.html.haml"
+    copy_file "not_found.html.haml", "app/views/errors/not_found.html.haml"
   end
 end
