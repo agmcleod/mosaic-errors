c6b624313491c430d840290348549d9c2a9b20af
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-09-18 12:20:37 -0400

    testing options.

--- lib/generators/errors_generator.rb
+++ lib/generators/errors_generator.rb
@@ -10,12 +10,7 @@ class ErrorsGenerator < Rails::Generators::NamedBase
 
     copy_file 'controllers/errors_controller.rb', 'app/controllers/errors_controller.rb'
     file_type = 'erb'
-    begin
-      if Haml
-        file_type = 'haml'
-      end
-    rescue NameError
-    end
+    raise options[:rails].inspect
     copy_file "views/errors/internal_server_error.html.#{file_type}", "app/views/errors/internal_server_error.html.#{file_type}"
     copy_file "views/errors/not_found.html.#{file_type}", "app/views/errors/not_found.html.#{file_type}"
     copy_file "views/errors/unprocessable_entity.html.#{file_type}", "app/views/errors/unprocessable_entity.html.#{file_type}"
